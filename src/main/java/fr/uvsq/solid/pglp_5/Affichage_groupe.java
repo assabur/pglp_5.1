c7cef0ca80aa18331cb9ad0a02dc418fd106e95c
Only_soft <abdullahwin99@gmail.com>
2020-04-03 20:57:41 +0200

    initialisation du projet 5.1

---
+++ src/main/java/fr/uvsq/solid/pglp_5/Affichage_groupe.java
@@ -0,0 +1,144 @@
+package fr.uvsq.solid.pglp_5;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+/*
+ * classe qui gere l'affichage par groupe
+ */
+public class Affichage_groupe implements java.io.Serializable
+{
+	/*
+	 * declaration des attributs
+	 */
+	private ArrayList<Interface_Personnel>tab=new ArrayList<Interface_Personnel>();
+	/*
+	 * methode qui prend en entré la racine de notre graphe d'hierarchie
+	 */
+	public Affichage_groupe (Interface_Personnel root)
+	{
+		this.tab.add(root);
+	}
+	
+	/*
+	 * methode qui retourne un nouveau iterateur
+	 */
+	public Iterator getIterator ()
+	{
+		return new GroupeIterator();
+		
+	}
+	private class GroupeIterator implements Iterator
+	{
+		/*
+		 * parcourir l'architecture en verifiant a chaque fois que l'instance est un composite ou pas
+		 */
+		public  GroupeIterator ()
+		{
+			int index =0;
+			while (index < tab.size())
+			{	
+				if	(tab.get(index) instanceof Composite_Personnel)
+				{	
+					 for (Interface_Personnel personne : ((Composite_Personnel)tab.get(index)).personnel )
+					{ 		      
+				          tab.add(personne);		
+				    }				
+				}				
+				index ++;
+			}
+		}
+		int i=0;
+
+		 /*
+		 * methode qui verifie si il existe un suivant dans la hierarchie
+		 */
+		public boolean hasNext() 
+		{
+			if (i < tab.size())			
+			 return true;			
+			 return false;
+		}
+
+		/*
+		 * methode qui renvoie le prochain personnel dans la hierarchie
+		 */
+		public Interface_Personnel Next() {
+			
+			if (this.hasNext()){
+				i++;
+			return tab.get(i-1);
+			}
+			return null;
+		}	
+	}
+		/* 
+		 * methode implementant la serialisation d'une hierarchie
+		 * on initialise notre fichier des valeurs de notre tableau contenant des personnes
+		 */
+		public void serializable_Personnel()
+		{		
+			 try {
+				 
+		         FileOutputStream fileOut = new FileOutputStream("/Ressources/personnel.pers");
+		         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		         int index =0;
+					while (index <tab.size())
+					{	
+						if	(tab.get(index) instanceof Composite_Personnel)
+						{	
+							 for (Interface_Personnel personne : ((Composite_Personnel)tab.get(index)).personnel )
+							{ 		      
+								 out.writeObject(((Composite_Personnel)tab.get(index)).personnel );		
+						    }				
+						}
+					   out.writeObject(((Composite_Personnel)tab.get(index)).personnel );	
+					   index ++;
+					}				
+					
+		         out.close();
+		         fileOut.close();  
+		      } 
+			 catch (IOException i) 
+			 {
+		    	  System.out.printf("erreur d'ecriture dans le fichier personnel");
+		         i.printStackTrace();
+		     }
+		}
+		
+		/* 
+		 * methode implementant la deserialisation d'une hierarchie
+		 * on recupere les personnes dans notre fichier 
+		 */
+		public void deserializable_Personnel()
+		{
+			Interface_Personnel personne;			
+				
+			try {
+				 FileInputStream fileIn = new FileInputStream("/Ressources/personnel.pers");
+		         ObjectInputStream in = new ObjectInputStream(fileIn);
+		         
+		         while(true)
+		         {
+		        	 personne =(Interface_Personnel) in.readObject();
+		        	 personne.affiche();
+		        	 in.close();
+				     fileIn.close();
+		         }
+		      } 
+			catch (IOException i)
+			  {
+				System.out.println("Terminé");
+		         i.printStackTrace(); 
+		      } 
+			 catch (ClassNotFoundException c) 
+			{
+		         System.out.println("personnel non trouvé");
+		         c.printStackTrace();
+		    }
+	   }
+					
+}
---
+++ src/main/java/fr/uvsq/solid/pglp_5/Personnels.java
@@ -0,0 +1,97 @@
+package fr.uvsq.solid.pglp_5;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+/*
+ * definition de la classe personnel
+ */
+public final class  Personnels implements Interface_Personnel  {
+	
+	/*
+	 * constructeur prend en entré un builder
+	 */
+	private Personnels(Builder builder)
+	{
+		  //parametres requis 
+		   id=builder.id;
+		   nom=builder.nom;
+		   prenom=builder.prenom;	 
+		  
+		  //parametre optionels
+		    telephone =builder.telephone;	  
+		    fonction=builder.fonction;
+		    date_naiss=builder.date_naiss;
+	}
+
+		/*
+		 * définition des parametres requis de notre builder
+		 */
+		 private int id;
+		 private String nom;
+		 private String prenom;
+		 private String fonction;
+		 private LocalDateTime date_naiss;
+		 private ArrayList<Integer> telephone =new ArrayList<Integer>();
+	/*
+	 * 	methode d'affichage du personnel de l'hierarchie
+	 */
+	public void affiche()
+	{
+		System.out.println("Personnel "+this.id+" :"+" Nom: "+this.nom+" Prenom: "+this.prenom);		
+	}
+	  /*
+	   * redefinissons la methode tostring
+	   */
+	 public String toString()
+	  {	 
+		 String chaine ="je suis le personnel "+this.id;
+		 return chaine;
+	  }
+	/*
+	 * definition de la classe  builder
+	 */
+  public static class Builder
+  {
+	  //parametres requis 
+	  private int id;
+	  private String nom;
+	  private String prenom;	 
+  
+	  //parametre optionels
+	  private LocalDateTime date_naiss;
+	  private ArrayList<Integer> telephone =new ArrayList<Integer>();	  
+	  private String fonction;
+	  
+	  /*
+	   * Constructeur du builder
+	   */
+	  public Builder(int id,String nom,String prenom)
+	  {
+		  this.id=id;
+		  this.nom=nom;
+		  this.prenom=prenom;  
+	  }
+	  /*
+	   * methode servant a modifier le parametre optionnel fonction
+	   */
+	  public Builder fonction_change(String new_fonction)
+	  {
+		  fonction=new_fonction;
+		  return this;  
+	  }
+	  /*
+	   * methode servant a modifier le parametre optionnel numero de telephone
+	   */
+	  public Builder telephone_change(ArrayList<Integer> new_telephone)
+	  {
+		  telephone=new_telephone;
+		  return this;    
+	  }
+	  /*
+	   * methode qui renvoie un nouveau personnel
+	   */
+	  public Personnels build()
+	  {
+		  return new Personnels(this);	  
+	  }
+  }
+}
